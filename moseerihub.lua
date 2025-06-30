bfe86251073a3c98928bd2b467877fd24fbc0152
moseeri01 <39209639+moseeri01@users.noreply.github.com>
2025-06-30 10:34:01 +0700

    Update moseerihub.lua

--- moseerihub.lua
+++ moseerihub.lua
@@ -1,22 +1,20 @@
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source.lua"))()
-
 local Window = Rayfield:CreateWindow({
-   Name = "moseeri hub",
-   LoadingTitle = "Moseeri Hub Loading....",
-   LoadingSubtitle = "by amira",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil,
-      FileName = "Big Hub"
-   },
-   KeySystem = true,
-   KeySettings = {
-      Title = "Moseeri Key",
-      Subtitle = "Get your key from Discord",
-      Note = "https://discord.gg/uGX2X3Wv",
-      FileName = "MoseeriKey",
-      SaveKey = true,
-      GrabKeyFromSite = true,
-      Key = {"https://raw.githubusercontent.com/moseeri01/key/main/key.txt"}
-   }
+    Name = "Moseeri Hub",
+    LoadingTitle = "Loading...",
+    LoadingSubtitle = "By Amira",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = nil,
+        FileName = "MoseeriHub"
+    },
+    KeySystem = true,
+    KeySettings = {
+        Title = "Moseeri Key",
+        Subtitle = "Get your key from Discord",
+        Note = "https://discord.gg/uGX2X3Wv",
+        FileName = "MoseeriKey",
+        SaveKey = true,
+        GrabKeyFromSite = true,
+        Key = {"https://raw.githubusercontent.com/moseeri01/key/main/key.txt"}
+    }
 })
